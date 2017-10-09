7a26387cdc87744869fe4164e3510dbf3021d45d
Hardik Chauhan <hardik.chauhan@solutionanalysts.com>
2017-10-09 11:06:18 +0530

    inital commit

---
+++ app/src/androidTest/java/com/sa/hardik/fingerprintauthenticationexample/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.sa.hardik.fingerprintauthenticationexample
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.sa.hardik.fingerprintauthenticationexample", appContext.packageName)
+    }
+}
